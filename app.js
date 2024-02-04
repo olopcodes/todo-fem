171c19836e8d407fe7e101f7fc0ee09b02d24e31
olopcodes <olopcodes@gmail.com>
2024-02-04 13:28:04 -0500

    making use of the data structure thoughout the app

--- app.js
+++ app.js
@@ -8,39 +8,43 @@ $(document).ready(function () {
     const todoStatsEl = $('.todo-stats');
     const todosLeftSpan = $('.todo-left span');
     const todoFiltersBoxEl = $('.todo-filter-box');
-    const data = [
-      // {
-      //   id: 1,
-      //   text: 'code',
-      //   completed: false,
-      // }
-    ];
+    const data = [];
 
     // listeners
 
-    // add todo handler
+    // add todo handler ===========================
+    $(themeBtnEl).click(function(e) {
+      toggleThemeColor();
+    })
+
+
     $(addTodoBtn).click(function (e) { 
         e.preventDefault();
         if(todoInput.val().length >= 3) {
+            // get value from input
             const newTodoText = todoInput.val();
 
-            // form todo html in this function
-            const newTodoEl = todoHMTL(newTodoText);
+            // create todo obj
+            const todoObj = {text: newTodoText, completed: false, id: Math.floor(Math.random() * 100000000000000)+1};
 
-            // add todo to data
-            
+            // add todo to dataobj to data
+            data.push(todoObj);
+
+            // form todo html in this function
+            const newTodoEl = todoHMTL(newTodoText, todoObj.id);
 
             // add new todo to the list
             $(todoListEl).append(newTodoEl);
 
-            // increase todos left
-            incrementTodosLeft();
+            // show todos left
+            getTodosLeft();
 
             // display stats and filter box
             showStatsAndFilters();
 
             // show drag text when more than 1 todo present
             toggleDragText();
+
         } else {
             alert('todo needs to be more than 3 characters')
         }
@@ -53,71 +57,114 @@ $(document).ready(function () {
       // capture button
       const buttonEl = $(e.target).closest('button');
 
+      // todo item
+      const todoItem = $(e.target).closest('li');
+
+      // get id 
+      const todoId = $(e.target).closest('li').attr('id');
+      
       // person clicked on check
       if($(buttonEl).hasClass('todo-check-btn')) {
 
-        $(buttonEl).parent().addClass('complete');
-        
-        decrementTodosLeft();
+        // if complete is present
+        if($(todoItem).hasClass('complete')) {
+
+          // remove complete class
+          $(buttonEl).parent().removeClass('complete');
+
+          // remove complete
+          updateCompletedTodos(todoId, false);
+
+          // unhide delete button
+          $(`#${todoId} .todo-delete-btn`).removeClass('hide');
+
+        } else {
+
+          // add complete class
+          $(buttonEl).parent().addClass('complete');
+
+          // add complete
+          updateCompletedTodos(todoId, true);
+          
+          // hide the delete btn
+          $(`#${todoId} .todo-delete-btn`).addClass('hide');
+          
+        }
+
+
       } else if ($(buttonEl).hasClass('todo-delete-btn')) {
+         // delete todo item from dom
+         $(buttonEl).parent().remove();
 
-        // person clikced on remove
-        $(buttonEl).parent().remove()
-
-        // decrease todos left
-        decrementTodosLeft();
-        
-        // remove drag text when less than 2 items left
+        //  remove todo from data
+        for(let i = data.length-1; i >= 0; i--) {
+          if(data[i].id === Number(todoId)) {
+            data.splice(i, 1);
+          }
+        }
+         
         toggleDragText();
+
       } else if ($(buttonEl).hasClass('todo-clear')) {
         // person clicked on clear todos
-        // get all complete todos
-        const completeTodos = ($('.todo-item.complete'));
+        
+        // remove completed from all todos in data
+        $.each(data, function (indexInArray, valueOfElement) { 
+           if(data[indexInArray].completed) {
+            // remove complete class
+            $(`#${data[indexInArray].id}.complete`).removeClass('complete');
 
-        // remove complete class
-        completeTodos.removeClass('complete')
-        
-        // update todos left
-        for(let todos = $('.todo-item').length; todos > 0; todos--) {
-          incrementTodosLeft();
-        }
-        
-        // loop over todos alternative
-        // let todos = $('.todo-item')
-        // $.each(todos, function (indexInArray, valueOfElement) { 
-        //    incrementTodosLeft();
-        // });
+            // change todo back to false
+            data[indexInArray].completed = false;
+
+            // unhide the delete btn
+            $(`#${data[indexInArray].id} .todo-delete-btn`).removeClass('hide');
+           }
+        });
+
+
       }
+
+      toggleDisbaledClass();
+
+      // show todos left
+      getTodosLeft();
     });
 
     $(todoFiltersBoxEl).click(function(e) {
-      let todos;
+      let todos = [];
       
       if($(e.target).attr('id') === 'todo-all') {
         // get all todos 
-        todos = $('.todo-item')
+        todos = data
+
+        console.log(todos)
 
       } else if($(e.target).attr('id') === 'todo-active') {
-      
-        // get todo items that do not have the complete class
-        todos = $('.todo-item').not('.complete');
+        // get active todos
+        for(let i = data.length-1; i >= 0; i--) {
+          if(!data[i].completed) {
+            todos.push(data[i]);
+          }
+        }  
+
 
       } else if($(e.target).attr('id') === 'todo-completed') {
 
         // get completed todos
-        todos = $('.todo-item.complete');
+        for(let i = data.length-1; i >= 0; i--) {
+          if(data[i].completed) {
+            todos.push(data[i]);
+          }
+        }  
       }
 
       let html = '';
       
-      // for(let i = todos.length; i >= 0; i--) {
-      //   console.log(todos[i])
-      // }
+      
       $.each($(todos), function (indexInArray, valueOfElement) { 
-         const text = $(todos[indexInArray]).text().trim();
-        console.log(text)
 
-          html += todoHMTL(text);
+          html += todoHMTL(todos[indexInArray].text);
       });
 
       // clear todoList
@@ -127,24 +174,23 @@ $(document).ready(function () {
     })
 
     // functions
-    function incrementTodosLeft() {
-      // increase nuumber of todos to be completed
-      todosLeft++;
-      // update todos left text
-      todosLeftSpan.text(todosLeft);
-    }
 
-    function decrementTodosLeft () {
-      if(todosLeft === 0) return;
-      // increase nuumber of todos to be completed
-      todosLeft--;
-      // update todos left text
-      todosLeftSpan.text(todosLeft);
+    function toggleThemeColor() {
+      if($('body').hasClass('light')) {
+        $('body').removeClass('light');
+        $('.todo-btn-theme img').attr('src', './images/icon-sun.svg');
+        $('body').attr('id', 'dark-theme');
+      } else {
+        $('body').addClass('light');
+        $('.todo-btn-theme img').attr('src', './images/icon-moon.svg');
+        $('body').attr('id', '');
+      }
     }
+    
 
-    function todoHMTL (text) { 
+    function todoHMTL (text, id) { 
         return `
-        <li class="todo-item">
+        <li id="${id}" class="todo-item">
         <button class="todo-check-btn todo-circle">
           <img src="./images/icon-check.svg" alt="check">
         </button>
@@ -157,8 +203,23 @@ $(document).ready(function () {
         `
      } 
 
+     function toggleDisbaledClass () {
+      let hasCompleteTodos = 0;
+
+      for(let i = data.length-1; i >= 0; i--) {
+        if(data[i].completed) hasCompleteTodos++;
+      }
+
+      if(hasCompleteTodos === 0 || data.length === 0) {
+        $('.todo-clear').addClass('disabled');
+      } else {
+        $('.todo-clear').removeClass('disabled');
+      }
+
+     }
+
     function toggleDragText() {
-      if(todosLeft >= 2) {
+      if(data.length >= 2) {
         $('.todo-drag-text').removeClass('hide')   
       } else {
         $('.todo-drag-text').addClass('hide')
@@ -171,5 +232,26 @@ $(document).ready(function () {
 
       // show filter box
       todoFiltersBoxEl.removeClass('hide');
+    }
+
+    function updateCompletedTodos (todoId, boolean) {
+      // update the data for complete todos
+      $.each(data, function (indexInArray, valueOfElement) { 
+        if(data[indexInArray].id === Number(todoId)) {
+          data[indexInArray].completed = boolean;
+        }
+      });
+    }
+
+    function getTodosLeft () {
+      let todosLeft = 0;
+
+      $.each(data, function (indexInArray, valueOfElement) { 
+          if(data[indexInArray].completed === false) {
+            todosLeft++;
+          }
+      });
+
+      todosLeftSpan.text(todosLeft);
     }
 });
